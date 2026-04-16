e461a945022dad4c9f8960ae57151aa520c99fa3
AzriVz <azri.pohan@gmail.com>
2026-04-16 08:21:58 +0700

    feat: added movecard

---
+++ src/models/Card/Skills/MoveCard.cpp
@@ -0,0 +1,33 @@
+#include "MoveCard.hpp"
+#include "Player.hpp"
+#include <iostream>
+
+using namespace std;
+
+static const int BOARD_SIZE = 40;
+
+MoveCard::MoveCard(const std::string &name, const std::string &description, int steps)
+    : SkillCard(name, description), steps(steps)
+{
+}
+
+MoveCard::~MoveCard() {}
+
+int MoveCard::getSteps() const
+{
+    return steps;
+}
+
+void MoveCard::executeAction(Player &player)
+{
+    int oldPos = player.getPosition();
+    int newPos = (oldPos + steps) % BOARD_SIZE;
+    player.setPosition(newPos);
+
+    cout << "MoveCard digunakan! Maju " << steps << " petak." << endl;
+    cout << "Bidak " << player.getUsername()
+         << " berpindah dari petak " << (oldPos + 1)
+         << " ke petak " << (newPos + 1) << "." << endl;
+
+    player.setUsedSkillThisTurn(true);
+}
