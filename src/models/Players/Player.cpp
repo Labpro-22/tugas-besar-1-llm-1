1d8895a1bba950d7245bb479df363c66c5d3bc05
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-04-19 10:56:04 +0700

    feat: player; unfinished

--- src/models/Players/Player.cpp
+++ src/models/Players/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.hpp"
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -7,3 +8,38 @@ Player::Player(int id, const string& username)
       hasUsedSkillThisTurn(false), hasRolledDice(false), consecutiveDoubles(0) {}
 
 Player::~Player() {}
+
+Player& Player::operator+=(int amount) {
+    money += amount;
+    return *this;
+}
+
+Player& Player::operator-=(int amount) {
+    money -= amount;
+    return *this;
+}
+
+int Player::getLiquidWealth() const {
+    return money;
+}
+
+vector<PropertyTile*> Player::getProperties() const {
+    return properties;
+}
+
+void Player::addProperty(PropertyTile* property) {
+    if (property == nullptr) {
+        return;
+    }
+
+    auto it = find(properties.begin(), properties.end(), property);
+    if (it == properties.end()) {
+        properties.push_back(property);
+    }
+}
+
+void Player::removeProperty(PropertyTile* property) {
+    properties.erase(remove(properties.begin(), properties.end(), property), properties.end());
+}
+
+// TODO: hasMonopoly check isMonopolyOwned in StreetTile
